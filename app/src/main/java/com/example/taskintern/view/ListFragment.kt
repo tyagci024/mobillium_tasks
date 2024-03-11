f393adf048c758a27ece6a5093c38d691b52b635
Turan Yağcı <tyagci024@gmail.com>
2024-03-11 14:09:28 +0300

    pr update

--- app/src/main/java/com/example/taskintern/view/ListFragment.kt
+++ app/src/main/java/com/example/taskintern/view/ListFragment.kt
@@ -27,7 +27,7 @@ class ListFragment : Fragment() {
 
         binding = FragmentListBinding.inflate(inflater, container, false)
         weatherList = mutableListOf(
-            Weather(0, "Ankara", "25°C", 10, 28, "Rüzgarlı"),
+            Weather(0, "Aydın", "25°C", 10, 28, "Rüzgarlı"),
             Weather(1, "İzmir", "20°C", 12, 29, "Bulutlu"),
             Weather(2, "İstanbul", "19°C", 11, 23, "Güneşli")
         )
